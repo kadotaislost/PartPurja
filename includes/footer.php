b880e363e2d68bb74768dff04904ba19ea0f7100
Sachin Lama <sachin2211068@iimscollege.edu.np>
2025-02-12 08:16:51 +0545

    start project

---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello everyone</title>
+</head>
+<body class = "bg-dark text-white">
+    <footer class = "container">
+        <p class = "text-center">All rights reserved &copy; 2020</p>
+    </footer>
+    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>hi</title>
+    <link href="assets\css\style.css" rel="stylesheet">
+</head>
+
+<body class = "bg-blue-400">
+    <h1 class ="text-8xl">hello everyone </h1>
+    <footer class = "container">
+        <p class = "text-center">All rights reserved &copy; 2020</p>
+    </footer>
+</body>
+</html>
